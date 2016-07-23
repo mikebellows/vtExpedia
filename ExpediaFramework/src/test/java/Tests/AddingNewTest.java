842e292c87c163d22aeffae8593e7f1fbec1e3fb
vt <vinodtharavathhere@gmail.com>
2016-07-23 18:04:54 +0530

    Adding a new junittest Case

---
+++ ExpediaFramework/src/test/java/Tests/AddingNewTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author vinodtharavath
+ *
+ */
+public class AddingNewTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		System.out.println("Hello world");
+		
+	}
+
+}
